15c07d2111cb826538e3ded835af281f7fac49fe
gabriel-roque <gabrielrms.dll@gmail.com>
2020-07-26 12:14:36 -0300

    basis type defs, querys and mutations graphql

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { getUsers, getUserByEmail, createUser } from './fake.services';
 
 // Em GraphQL toda request é do tipo POST
 // Toda request bate no MESMO endpoint (/graphql)
@@ -10,16 +11,67 @@ import { ApolloServer, gql } from 'apollo-server';
 // Querys e Mutations são string enviadas no corpo da requisição
 
 const typeDefs = gql`
+  # Quando possui o <tipo>! estamos dizendo que é obrigatório
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
+  }
+
+  # Usa-se  [User!]! para não permitir que a api retorne um array com null
   type Query {
     hello: String
+    users: [User!]!
+    getUserByEmail(email: String!): User!
+  }
+
+  type Mutation {
+    createUser(name: String, email: String): User!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello Wolrd',
+    users: () => getUsers(),
+    getUserByEmail: (_, args) => getUserByEmail(args.email),
+  },
+
+  Mutation: {
+    createUser: (_, args) => createUser(args.name, args.email),
   },
 };
+
+// Query para obter por um email
+/* 
+query {
+  user: getUserByEmail(email: "gabriel@gmail.com") {
+    name,
+    email,
+    active
+  }
+}
+ */
+
+//  Mutation de criar usuario
+/* 
+ mutation {
+  user: createUser(name: "Camila", email: "camila@gmail.com") {
+    _id
+    email
+    name
+    active
+  }
+}
+ */
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => console.log(`Server started ar ${url}`));
