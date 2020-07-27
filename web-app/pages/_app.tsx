0b609ff6c9e24138de2e972515bb4eeadc58c960
gabriel-roque <gabrielrms.dll@gmail.com>
2020-07-27 17:19:18 -0300

    CRUD nextjs + graphql

--- web-app/pages/_app.tsx
+++ web-app/pages/_app.tsx
@@ -1,6 +1,13 @@
 import App from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -19,9 +26,11 @@ export default class MyApp extends App {
     return (
       <>
         <Head>
-          <title>My new cool app</title>
+          <title>NestJS + GraphQL</title>
         </Head>
-        <Component {...pageProps} />
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </>
     );
   }
--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -1,23 +1,112 @@
-import axios from 'axios';
-import { gql } from 'apollo-server-micro';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import { useState, useEffect } from 'react';
+
+const GET_USERS = gql`
+  query {
+    users {
+      id: _id
+      fullName
+    }
+  }
+`;
+
+const CREATE_USER = gql`
+  mutation CreateUser($firstName: String!, $lastName: String!, $email: String) {
+    user: createUser(
+      data: {
+        firstName: $firstName
+        lastName: $lastName
+        email: $email
+        active: true
+      }
+    ) {
+      id: _id
+      fullName
+    }
+  }
+`;
+
+const DELETE_USER = gql`
+  mutation DeleteUser($id: ID!) {
+    deleteUser(id: $id)
+  }
+`;
 
 export default function Index() {
-  const queryUsers = gql`
-    query {
-      users {
-        fullName
-      }
+  const [users, setUsers] = useState([
+    { id: '', firstName: '', lastName: '', email: '', fullName: '' },
+  ]);
+  const [form, setForm] = useState({ firstName: '', lastName: '', email: '' });
+  const { data } = useQuery(GET_USERS);
+  const [createUser, { data: userCreated }] = useMutation(CREATE_USER);
+  const [deleteUser] = useMutation(DELETE_USER);
+
+  useEffect(() => {
+    if (data !== undefined) setUsers(data.users);
+  }, [data]);
+
+  useEffect(() => {
+    if (userCreated !== undefined) {
+      setUsers([...users, userCreated.user]);
     }
-  `;
+  }, [userCreated]);
 
-  async function getUsers() {
-    await axios
-      .post('http://localhost:4000', { query: queryUsers.loc.source.body })
-      .then((resp) => {
-        console.log(resp.data);
-      });
+  function popUserList(user) {
+    setUsers(users.filter((_user) => _user.id !== user.id));
   }
-  getUsers();
 
-  return <div>User</div>;
+  return (
+    <div>
+      <ul>
+        {users.map((user, i) => (
+          <div key={i}>
+            <li>{user.fullName}</li>
+            <button
+              onClick={() => {
+                deleteUser({ variables: { id: user.id } });
+                popUserList(user);
+              }}
+            >
+              DELETE
+            </button>
+          </div>
+        ))}
+      </ul>
+
+      <hr />
+
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            createUser({ variables: { ...form } });
+            setForm({ firstName: '', lastName: '', email: '' });
+          }}
+        >
+          <input
+            type="text"
+            placeholder="First Name"
+            value={form.firstName}
+            onChange={(e) => setForm({ ...form, firstName: e.target.value })}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={form.lastName}
+            onChange={(e) => setForm({ ...form, lastName: e.target.value })}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Email"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+          />
+          <br />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
